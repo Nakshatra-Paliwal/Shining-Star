6b65be0c9f530543d6a6937795aa69f8088ce4a8
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-28 20:33:13 +0530

    Add files via upload

---
+++ My Mark Sheet.py
@@ -0,0 +1,19 @@
+name = "Nakshatra Paliwal"
+rollnumber = 9019
+english = "73/80"
+hindi = "77/80"
+maths = "68/80"
+science = "73/80"
+socialscience = "78/80"
+computer = "40/40"
+percentage = "92%"
+print (name)
+print (rollnumber)
+print (english)
+print (hindi)
+print (maths)
+print (science)
+print (socialscience)
+print (computer)
+print (percentage)
+
---
+++ STATISTICS OF CRICKET WORLD CUP.py
@@ -0,0 +1,14 @@
+# STATISTICS OF CRICKET WORLD CUP #
+
+NumOfCountries = 20
+WorldCupStartYr = "7 June 1975"
+HowManyWorldCupsTillDt = 12
+MostSuccessfulCountry = "Australia"
+print(NumOfCountries)
+print(WorldCupStartYr)
+print(HowManyWorldCupsTillDt)
+print(MostSuccessfulCountry)
+
+
+
+
