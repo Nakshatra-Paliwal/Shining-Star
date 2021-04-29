971d5e25587263eda43a83c94f3b5568db6bd7b5
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-29 20:48:51 +0530

    Add files via upload

---
+++ Where do you live.py
@@ -0,0 +1,13 @@
+city = input("In which city do you stay? ")
+if city == "Pune" :
+    print("Cool place in Maharashtra")
+elif city == "Chennai":
+    print("Hot city but Cool place!!")
+elif city == "Mumbai" :
+    print("You are living in a business hub")
+elif city == "Sendhwa":
+    print("Awesome, Isn't it the city where Nakshatra Paliwal also lives!!")
+elif city == "Indore" :
+    print("Cool,You are Living in the Cleanest City of India!! ")    
+else:
+    print("Wow!!" + city + "is an Awesome place to live")    
