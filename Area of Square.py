ab861948891048c22efafd515c00cd8dd08cc058
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-30 15:01:50 +0530

    Add files via upload

---
+++ Area of Square.py
@@ -0,0 +1,3 @@
+side = int(input("Please enter the value of Side of the Square : "))
+Area = (side ** 2)
+print("Area of the Square is " + str(Area))
---
+++ Conversion of Metre to Kilometre.py
@@ -0,0 +1,3 @@
+m = int(input("Please enter the value of Metre : "))
+km = (m / 1000)
+print("Conversion of Metre to Kilometre is : " + str(km))
