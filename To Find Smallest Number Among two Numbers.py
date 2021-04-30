1dda14745777f97633d7f3442572b676a312e6db
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-30 17:35:11 +0530

    Add files via upload

---
+++ To Find Smallest Number Among two Numbers.py
@@ -0,0 +1,7 @@
+num1= int(input("Enter 1st number: "))
+num2= int(input("Enter 2nd number: "))
+print("")
+if num1<num2:
+    print("Number 1 is smaller") 
+else:
+    print("Number 2 is smaller")    
