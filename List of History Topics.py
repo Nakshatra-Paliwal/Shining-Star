5aee45cc00f38e1061c9836a35f9c279acc588da
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-30 22:35:30 +0530

    Add files via upload

---
+++ List of History Topics.py
@@ -0,0 +1,20 @@
+print("Please choose any one History Topic :- ")
+print("1. Great Depression History.")
+print("2. World War I ")
+print("3. Battle of the Somme.")
+print("4. American Revolution ")
+
+choice = int(input("Enter your choice:"))
+
+
+if choice==1:
+    print("The Great Depression was the worst economic downturn in the history of the industrialized world, lasting from the stock market crash of 1929 to 1939")
+    
+if choice==2:
+    print("World War I, also known as the Great War, began in 1914 after the assassination of Archduke Franz Ferdinand of Austria.")    
+    
+if choice==3:
+    print("The Battle of the Somme which took place from July to November 1916,began as an Allied offensive against German forces on the Western Front and turned into one of the most bitter and costly battles of World War I.")
+    
+if choice==4:
+    print("The Revolutionary War waged by the American colonies against Britain influenced political ideas and revolutions around the globe, as a small fledgling nation won its freedom from the greatest military force of its time.")
