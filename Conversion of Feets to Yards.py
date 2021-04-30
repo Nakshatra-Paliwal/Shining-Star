cb20e92abf5049ac59e9ccdc47419549e7c56997
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-30 16:08:32 +0530

    Add files via upload

---
+++ Conversion of Feets to Yards.py
@@ -0,0 +1,3 @@
+Feets = float(input("Please enter the value of Feets : "))
+Yards = (Feets / 3)
+print("Conversion of Feets to Yards is : " + str(Yards))
---
+++ To Find Percentage of Students.py
@@ -0,0 +1,8 @@
+Marks1 = float( input("Enter Marks for Subject 1 : "))
+Marks2 = float( input("Enter Marks for Subject 2 : "))
+Marks3 = float( input("Enter Marks for Subject 3 : "))
+Marks4 = float( input("Enter Marks for Subject 4 : "))
+Marks5 = float( input("Enter Marks for Subject 5 : "))
+Percentage = ((Marks1+Marks2+Marks3+Marks4+Marks5) / 500)*100
+print()
+print("Percentage: "+ str(Percentage) )
