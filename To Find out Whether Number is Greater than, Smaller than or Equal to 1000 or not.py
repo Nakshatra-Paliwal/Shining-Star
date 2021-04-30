97266db54d6f43c8856a4356583beb329e2acfc8
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-04-30 17:53:30 +0530

    Add files via upload

---
+++ To Find out Whether Number is Greater than, Smaller than or Equal to 1000 or not.py
@@ -0,0 +1,7 @@
+Num = int(input("Enter a Number : "))
+if Num < 1000 :
+    print("Your Number is Smaller Than 1000")
+if Num > 1000 :
+    print("Your Number is Greater Than 1000")
+if Num == 1000 :
+    print("Your Number is equal to 1000")
