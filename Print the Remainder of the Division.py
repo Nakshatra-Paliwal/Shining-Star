55443abc9c8262bdca085d52019697f2597442b0
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-05-02 21:17:20 +0530

    Add files via upload

---
+++ Print the Remainder of the Division.py
@@ -0,0 +1,9 @@
+"""
+Write a code to store two values in variables ‘num1’ & ‘num2’. 
+And print the remainder of the division of num1
+
+"""
+num1 = 18
+num2 = 5
+remain = num1 % num2 
+print (remain)
---
+++ To Print Multiplication of Two Numbers.py
@@ -0,0 +1,5 @@
+#Write a code to print Multiplication of two numbers
+num1 = 12
+num2 = 2
+multiply = num1 * num2
+print(multiply)
