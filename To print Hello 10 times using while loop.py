83d367399c3282af741ef8bf9d70feb8024e050d
Nakshatra-Paliwal <83232811+Nakshatra-Paliwal@users.noreply.github.com>
2021-05-12 15:52:39 +0530

    Add files via upload

---
+++ To print Hello 10 times using while loop.py
@@ -0,0 +1,7 @@
+"""
+To print Hello 10 times using while loop
+"""
+i=0
+while i<10:
+    i=i+1
+    print("Hello")
---
+++ Using time lag Library.py
@@ -0,0 +1,9 @@
+"""
+Using time lag Library
+"""
+import time
+print("Covid-19 is a virus.")
+time.sleep(5)
+print("It is developed in 2019.")
+time.sleep (5)
+print("It came from China.")
